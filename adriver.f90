f81ea94437f55bbc801726947a53e8dba5786328
Toby The Bear <bear_tt@gsc_218_1.mercer.edu>
2020-04-11 10:54:06 -0400

    adding command line arguments to adriver

--- adriver.f90
+++ adriver.f90
@@ -13,12 +13,19 @@ real (kind=8) :: cputime
 real (kind=8) :: mflops, mflops2
 external walltime, cputime
 
+character (len=8) :: carg1, carg2, carg3
 
+call get_command_argument(1, carg1)
+call get_command_argument(2, carg2)
+call get_command_argument(3, carg3)
 
-read *, startval, stopval, stepval
- 
+! Use Fortran internal files to convert command line arguments to ints
+
+read (carg1,'(i8)') startval
+read (carg2,'(i8)') stopval
+read (carg3,'(i8)') stepval
+
 do iter = startval, stopval, stepval
-  
 
 N = iter
 
